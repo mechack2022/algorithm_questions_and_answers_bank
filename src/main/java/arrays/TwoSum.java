5585c1ec5edff94f7ffbd63199e0443d3b1596b1
mechack2022 <dtoms624@gmail.com>
2023-10-12 07:09:53 +0100

    modified

--- src/main/java/arrays/TwoSum.java
+++ src/main/java/arrays/TwoSum.java
@@ -12,7 +12,7 @@ Example: Input: nums = [2, 7, 11, 15], target = 9. Output: [0, 1].*/
 public class TwoSum {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(twoSum4(new int[]{ 2, 7, 11, 15}, 9)));
+        System.out.println(Arrays.toString(twoSum3(new int[]{ 2, 7, 11, 15}, 22)));
     }
 
     public static int[] twoSum(int[] arr, Integer target) {
